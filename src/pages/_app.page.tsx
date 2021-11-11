0eb2ba8ca257be0cba52bdfadf571a4934a55c2e
Jimmy <jimmy@Jimmys-Mac-mini.home>
2021-11-11 12:05:36 +0100

    add TW and App entrypoint

---
+++ src/pages/_app.page.tsx
@@ -0,0 +1,6 @@
+import '../styles/compiled.css';
+import type { AppProps } from 'next/app';
+
+const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+
+export default App;
